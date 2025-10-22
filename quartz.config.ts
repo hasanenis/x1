2520cbf20f5c0d0141b97750ecca1ed6805bf4c3
hasanenis <seningithubmailin@example.com>
2025-10-22 09:12:58 +0300

    Set baseUrl to hasanenis.github.io/x1.git

--- quartz.config.ts
+++ quartz.config.ts
@@ -1,4 +1,4 @@
-import { QuartzConfig } from "./quartz/cfg"
+﻿import { QuartzConfig } from "./quartz/cfg"
 import * as Plugin from "./quartz/plugins"
 
 /**
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "hasanenis.github.io/x1.git",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
@@ -95,3 +95,4 @@ const config: QuartzConfig = {
 }
 
 export default config
+
